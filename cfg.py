1634149e976599e8f43a15321a76f7ca13b44c5c
akhomlyuk <relicbs@gmail.com>
2023-09-10 00:38:31 +0300

    v0.1

--- cfg.py
+++ cfg.py
@@ -1,4 +1,4 @@
 from aiogram import Bot
 
-bot_token = '6686016703:AAEDpaP6ulcH_6fU5QV-e7ysCYnzAfq5iKA'
+bot_token = ''
 bot = Bot(token=bot_token, parse_mode="HTML")
--- shell.py
+++ shell.py
@@ -28,6 +28,7 @@ async def shell_state(message: Message, state: FSMContext):
 @router.message(Shell.ShellOn)
 async def cmd_shell(message: Message, state: FSMContext):
     try:
+        # subprocess.Popen('chcp 65001', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         await state.update_data(shell_cmd=message.text.strip())
 
         command = message.text.strip()
@@ -39,7 +40,7 @@ async def cmd_shell(message: Message, state: FSMContext):
             part_size = 3900
             message_parts = [output[i:i + part_size] for i in range(0, len(output), part_size)]
             for part in message_parts:
-                await message.answer(f"{part}")
+                await message.answer(f"{part}", parse_mode=None)
 
         if command.lower() == 'exit':
             await state.clear()
